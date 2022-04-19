7542af92edcc621b56c58a5f7e7b8a9999b099cc
unknown <birdzhan.99@msn.com>
2022-04-19 13:00:58 +0200

    modification de controleur Connexion

--- controleur/controllerConnexion.php
+++ controleur/controllerConnexion.php
@@ -2,18 +2,49 @@
 
 require "../modele/PersonneManager.php";
 
-function verfierConnexion()
+function verifierConnexion($identifiant,$mdp)
 {
     $res = null;
     $personne = new PersonneManager();
 
-    if (!$personne->getPersonneAvecMdp()) {
+    if (!$personne->getPersonneAvecMdp($identifiant,$mdp)) {
         $res = "identifiant/mdp incorrect";
     }
+
     return $res;
 }
 
-function creerSession()
-{
-    # code...
+
+/*****************************************************/
+
+if (isset($_SESSION["sessionUtilisateur"])) {
+    // pas complet ?  
+    header("Location : ");
 }
+
+
+if (isset($_POST["submitConnexion"]) 
+    && isset($_POST["identifiant"])
+    && isset($_POST["mdp"])) {
+    
+
+
+    $personne = verifierConnexion(htmlspecialchars($identifiant),htmlspecialchars($mdp));
+    
+    if($personne){
+        $_SESSION["sessionUtilisateur"] = $personne;
+    }
+
+
+
+    
+}
+
+
+
+
+
+
+
+
+
--- modele/Personnel.php
+++ modele/Personnel.php
@@ -9,4 +9,7 @@ class Personnel{
         $this->identifiantRole = $identifiantRole;
     }
 }
+
+
+
 ?>
