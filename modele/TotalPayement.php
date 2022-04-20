92115a0779cc7690111f3cda1c319d9d62a636b8
Adam Mourahib Samir <la207263@student.helha.be>
2022-04-20 11:03:56 +0200

    modele TotalPayement ajoute, une modification a faire

---
+++ modele/TotalPayement.php
@@ -0,0 +1,23 @@
+<?php
+require_once('DBManager.php');
+class totalPayement extends DBManager
+{
+    public function listerTotalPayement()
+    {
+        
+        $sql="SELECT SUM(montant_paiement) FROM paiement WHERE identifiant_Personne=1";//a modifier id pour le reprendre de l'url
+        
+        try {
+            $con = $this->getConnexion();
+            $requete = $con->prepare($sql);
+            $requete->execute();
+
+        } catch (PDOException $e) {
+            $msgErreur = $e->getMessage();
+            $this->_vue = new Vue('Erreur');
+            $this->_vue->generer(array('msgErreur' => $msgErreur));
+        } 
+    }
+}
+
+?>
