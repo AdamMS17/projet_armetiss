606042e1307b43e0cbca3fcdc8f78b88efa031a3
Adam Mourahib Samir <la207263@student.helha.be>
2022-04-21 09:44:07 +0200

    Ajout verification TotalPayement pour verifier que l'utilisateur a bien effectue un payement

--- modele/TotalPayement.php
+++ modele/TotalPayement.php
@@ -6,17 +6,25 @@ class totalPayement extends DBManager
     {
         
         $sql="SELECT SUM(montant_paiement) FROM paiement WHERE identifiant_Personne=1";//a modifier id pour le reprendre de l'url
-        
+        $sqlVerif="SELECT montant_Paiement FROM paiement WHERE identifiant_Personne=1";
         try {
             $con = $this->getConnexion();
             $requete = $con->prepare($sql);
             $requete->execute();
             $res = $requete->fetch();
-            return $res[0];
+            $requeteVerif = $con ->prepare($sqlVerif);
+            $requeteVerif->execute();
+            $verif=$requeteVerif->fetch();
         } catch (PDOException $e) {
             $msgErreur = $e->getMessage();
             $this->_vue = new Vue('Erreur');
             $this->_vue->generer(array('msgErreur' => $msgErreur));
+        }
+        if ($verif<1) {
+            echo("Pas de paiement(s) de la part de cet utilisateur");
+        }
+        else {
+            return $res[0];
         } 
     }
 }
