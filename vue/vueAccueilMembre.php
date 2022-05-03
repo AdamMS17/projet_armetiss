d5a41840ac34f8d2fc176c1117d6a09380b1ee5c
unknown <birdzhan.99@msn.com>
2022-05-03 09:19:13 +0200

    ajout vueAccueilMembre

---
+++ vue/vueAccueilMembre.php
@@ -0,0 +1,15 @@
+<div>
+    <img src="" alt="image de profil">
+    <h3>nom : </h3>
+    <h3>prenom : </h3>
+    <h3>role : membre</h3>
+
+    <a href="Deconnexion.php">Se deconnecter</a>
+
+</div>
+
+<div>
+    <a href="">Voir mes Scéances</a>
+    <a href="">Voir les activités inscrits</a>
+    <a href="">Ma solde</a>
+</div>
