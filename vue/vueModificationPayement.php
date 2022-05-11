fde7ef93d697a4861f697984ec2529c05bf72385
Adam Mourahib Samir <la207263@student.helha.be>
2022-05-11 22:28:12 +0200

    ajout de vueModificationPayement et correction de vueAjouterPayement

--- vue/vueModificationPayement.php
+++ vue/vueModificationPayement.php
@@ -1,6 +1,6 @@
-<form action="" method="post" id='ajoutPayement' name="formAjoutPayement">
+<form action="" method="post" id='modifPayement' name="formModifPayement">
     <fieldset class="m-5">
-        <legend>Ajouter un nouveau payement pour une activite </legend>
+        <legend>Modifier le payement d'un eleve pour une activite </legend>
         <div class="mb-3">
             <label class="form-label" for="nomPersonne">Selectionnez un eleve</label>
             <select name="eleve" id="eleve">
@@ -8,10 +8,10 @@
             </select>
         </div>
         <div class="mb-3">
-            <label class="form-label" for="montantDuPayement">Montant du payement</label>
+            <label class="form-label" for="montantDuPayement">Nouveau montant total</label>
             <input class="form-control" type="text" id="montantDuPayement" name="montantDuPayement" required />
         </div>
                 
-        <input class="btn btn-success" type="submit" name="enregistrementAjoutPayement">
+        <input class="btn btn-success" type="submit" name="enregistrementModifPayement">
     </fieldset>
 </form>
