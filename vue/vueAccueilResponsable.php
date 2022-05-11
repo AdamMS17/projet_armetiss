4c1d8eaaa83f6792212a5cf6d08a13d63d24550c
Natteo Scannella <la190626@student.helha.be>
2022-05-11 22:39:27 +0200

    Rename 'error' files.

---
+++ vue/vueAccueilResponsable.php
@@ -0,0 +1,23 @@
+<div class="m-5">
+    <a href="?ajoutMembre=nouveauMembre">
+        Nouveau membre
+    </a>
+</div>
+
+<div class="m-5">
+    <a href="?inscriptionActivite=inscriptionActivite">
+        Inscription à une activité
+    </a>
+</div>
+
+<div class="m-5">
+    <a href="?inscriptionEvenement=inscriptionEvenement">
+        Inscription à un évènement
+    </a>
+</div>
+
+<div class="m-5">
+    <a href="?enregistrementPaiement=enregistrementPaiement">
+        Enregistrer un paiement
+    </a>
+</div>
