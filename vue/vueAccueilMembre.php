2bfbb7010ae8916eba06b77436a3a82fea9ca06c
Adam Mourahib Samir <la207263@student.helha.be>
2022-05-16 20:53:00 +0200

    Correction fautes d'ortoghraphe vueAccueilMembre

--- vue/vueAccueilMembre.php
+++ vue/vueAccueilMembre.php
@@ -1,15 +1,15 @@
 <div>
     <img src="" alt="image de profil">
-    <h3>nom : </h3>
-    <h3>prenom : </h3>
-    <h3>role : membre</h3>
+    <h3>Nom : </h3>
+    <h3>Prenom : </h3>
+    <h3>Rôle : membre</h3>
 
     <a href="Deconnexion.php">Se deconnecter</a>
 
 </div>
 
 <div>
-    <a href="">Voir mes Scéances</a>
-    <a href="">Voir les activités inscrits</a>
-    <a href="">Ma solde</a>
+    <a href="">Voir mes Séances</a>
+    <a href="">Voir les activités aux quelles je suis inscrit</a>
+    <a href="">Mon solde</a>
 </div>
